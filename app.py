a94e4cf6ad17ee2991b54212d97649ad672c4a17
Elaine French <elainekfrench@gmail.com>
2024-03-26 15:35:37 -0600

    db crreate route created

--- app.py
+++ app.py
@@ -7,7 +7,24 @@ def hello_world():
     return 'Hello World from Elaine French in 3008'
 
 @app.route('/db_test')
-def db_test:
+def db_test():
     conn = psycopg2.connect("postgres://lab10_database_92pn_user:tZV2KiVcJT0fA9VB1hN1xbVdXw1CzT5n@dpg-co1jr2o21fec73d362s0-a/lab10_database_92pn")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://lab10_database_92pn_user:tZV2KiVcJT0fA9VB1hN1xbVdXw1CzT5n@dpg-co1jr2o21fec73d362s0-a/lab10_database_92pn")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfully created"
